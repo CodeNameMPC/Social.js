6c341df21a6e443598f074fffdf9647623fb2c9f
= <mpcymerman@gmail.com>
2022-12-31 10:37:03 -0600

    Stripped controllers/post code - starting over. Added error handling middleware to index.js. added call to error handler from user controller

--- controllers/user.ts
+++ controllers/user.ts
@@ -2,155 +2,168 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import User from "../models/user.js";
+import express from "express";
 
 dotenv.config();
 
-export const create = async (req, res) => {
-  const {
-    _id,
-    loginType,
-    name,
-    image,
-    biography,
-    website,
-    email,
-    password,
-    confirmedPassword,
-  }: {
-    _id?: string;
-    loginType: string;
-    name: string;
-    image: string;
-    biography: string;
-    website: string;
-    email: string;
-    password?: string;
-    confirmedPassword?: string;
-  } = req.body;
-
-  let userData = {};
-  let existingUser;
-
-  // if we are creating via email, we will need to generate an id. we will get the user id from google for google creation
-  if (loginType.toUpperCase() == "EMAIL") {
-    existingUser = User.findOne({ email: email });
-    if (existingUser)
-      return res
-        .status(400)
-        .json({ message: "A user with that email already exists" });
-
-    if (password === confirmedPassword)
-      return res.status(400).json({ message: "Passwords do not match." });
-
-    const hashedPassword = await bcrypt.hash(password, 12);
-
-    userData = {
+export const create = async (req, res, next) => {
+  try {
+    const {
+      _id,
+      loginType,
       name,
       image,
       biography,
       website,
-      password: hashedPassword,
-      profileType: loginType,
-    };
-  } else if (loginType.toUpperCase() == "GOOGLE") {
-    existingUser = User.findOne({ email: email });
-    if (existingUser)
-      return res.status(400).json({
-        message: "A user associated with that Google account already exists",
-      });
+      email,
+      password,
+      confirmedPassword,
+    }: {
+      _id?: string;
+      loginType: string;
+      name: string;
+      image: string;
+      biography: string;
+      website: string;
+      email: string;
+      password?: string;
+      confirmedPassword?: string;
+    } = req.body;
 
-    userData = {
+    let userData = {};
+    let existingUser;
+
+    // if we are creating via email, we will need to generate an id. we will get the user id from google for google creation
+    if (loginType.toUpperCase() == "EMAIL") {
+      existingUser = User.findOne({ email: email });
+      if (existingUser)
+        return res
+          .status(400)
+          .json({ message: "A user with that email already exists" });
+
+      if (password === confirmedPassword)
+        return res.status(400).json({ message: "Passwords do not match." });
+
+      const hashedPassword = await bcrypt.hash(password, 12);
+
+      userData = {
+        name,
+        image,
+        biography,
+        website,
+        password: hashedPassword,
+        profileType: loginType,
+      };
+    } else if (loginType.toUpperCase() == "GOOGLE") {
+      existingUser = User.findOne({ email: email });
+      if (existingUser)
+        return res.status(400).json({
+          message: "A user associated with that Google account already exists",
+        });
+
+      userData = {
+        _id,
+        name,
+        image,
+        biography,
+        website,
+        profileType: loginType,
+      };
+    } else {
+      return res
+        .status(500)
+        .json({ message: `ERROR. Unknown login type ${loginType}` });
+    }
+
+    const user = User.create(userData);
+
+    return res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const login = async (req, res, next) => {
+  try {
+    const {
+      email,
+      password,
+      loginType,
+      _id,
+    }: { email?: string; password?: string; loginType: string; _id?: string } =
+      req.body;
+
+    let existingUser;
+
+    if (loginType.toUpperCase() == "GOOGLE")
+      existingUser = await User.findOne({ _id });
+    else if (loginType.toUpperCase() == "EMAIL")
+      existingUser = await User.findOne({ email });
+    else
+      return res
+        .status(500)
+        .json({ message: `ERROR. Unknown login type ${loginType}` });
+
+    if (!existingUser)
+      res.status(404).json({ message: "user does not exist with that email" });
+
+    if (loginType.toUpperCase() == "EMAIL") {
+      const isPasswordCorrect = await bcrypt.compare(
+        password,
+        existingUser.password
+      );
+
+      if (!isPasswordCorrect)
+        return res
+          .status(400)
+          .json({ message: `ERROR. Unknown login type ${loginType}` });
+    }
+
+    const token = jwt.sign(
+      { email: existingUser.email, id: existingUser._id },
+      process.env.JWT_SECRET
+    );
+
+    res.status(200).json({ result: existingUser, token });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const update = async (req, res, next) => {
+  try {
+    const {
       _id,
       name,
       image,
       biography,
       website,
-      profileType: loginType,
-    };
-  } else {
-    return res
-      .status(500)
-      .json({ message: `ERROR. Unknown login type ${loginType}` });
-  }
+    }: {
+      _id: string;
+      _type: string;
+      name: string;
+      image?: string;
+      biography?: string;
+      website?: string;
+    } = req.body;
 
-  const user = User.create(userData);
-
-  return res.status(200).json(user);
-};
-
-export const login = async (req, res) => {
-  const {
-    email,
-    password,
-    loginType,
-    _id,
-  }: { email?: string; password?: string; loginType: string; _id?: string } =
-    req.body;
-
-  let existingUser;
-
-  if (loginType.toUpperCase() == "GOOGLE")
-    existingUser = await User.findOne({ _id });
-  else if (loginType.toUpperCase() == "EMAIL")
-    existingUser = await User.findOne({ email });
-  else
-    return res
-      .status(500)
-      .json({ message: `ERROR. Unknown login type ${loginType}` });
-
-  if (!existingUser)
-    res.status(404).json({ message: "user does not exist with that email" });
-
-  if (loginType.toUpperCase() == "EMAIL") {
-    const isPasswordCorrect = await bcrypt.compare(
-      password,
-      existingUser.password
+    const user = await User.updateOne(
+      { _id },
+      {
+        name,
+        biography,
+        website,
+        image,
+      }
     );
 
-    if (!isPasswordCorrect)
-      return res
-        .status(400)
-        .json({ message: `ERROR. Unknown login type ${loginType}` });
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
   }
-
-  const token = jwt.sign(
-    { email: existingUser.email, id: existingUser._id },
-    process.env.JWT_SECRET
-  );
-
-  res.status(200).json({ result: existingUser, token });
 };
 
-export const update = async (req, res) => {
-  const {
-    _id,
-    name,
-    image,
-    biography,
-    website,
-  }: {
-    _id: string;
-    _type: string;
-    name: string;
-    image?: string;
-    biography?: string;
-    website?: string;
-  } = req.body;
-
-  const user = await User.updateOne(
-    { _id },
-    {
-      name,
-      biography,
-      website,
-      image,
-    }
-  );
-
-  res.status(200).json(user);
-};
-
-export const getUserByID = async (req, res) => {
+export const getUserByID = async (req, res, next) => {
   const { id } = req.params;
 
   try {
@@ -160,6 +173,6 @@ export const getUserByID = async (req, res) => {
 
     res.status(200).json(result);
   } catch (error) {
-    console.log(error);
+    next(error);
   }
 };
