ac1dbd90fd2fea64ee7f36c8289303207cfc2a9f
= <mpcymerman@gmail.com>
2023-01-01 16:27:06 -0600

    Had some Ephanies on user creation. Documented those in code and made the few changes. Added a constants/errorCodes.ts file to keep similar error messages universal

---
+++ constants/errorCodes.ts
@@ -0,0 +1,8 @@
+
+// USER
+export const USER_NOT_FOUND = "User with that information was not found"
+export const PASSWORDS_NOT_MATCH = "Passwords do not match"
+export const PASSWORDS_INCORRECT = "Password is incorrect"
+export const USER_EXISTS = "A user with that information already exists"
+
+// POST
